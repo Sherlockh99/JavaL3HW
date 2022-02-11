fbff62ae852ae36f2db769290acd48e8d9e556cd
Sherlockh99 <sherlockh989@gmail.com>
2022-02-11 18:33:49 +0200

    HW5

---
+++ src/main/java/lesson5/Car.java
@@ -0,0 +1,43 @@
+package lesson5;
+
+import static lesson5.MainClass.*;
+
+public class Car implements Runnable {
+    private static int CARS_COUNT;
+    private Race race;
+    private int speed;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+    public int getSpeed() {
+        return speed;
+    }
+    public Car(Race race, int speed) {
+        this.race = race;
+        this.speed = speed;
+        CARS_COUNT++;
+        this.name = "Участник #" + CARS_COUNT;
+    }
+    @Override
+    public void run() {
+        try {
+            System.out.println(this.name + " готовится");
+            Thread.sleep(500 + (int)(Math.random() * 800));
+            System.out.println(this.name + " готов");
+            cd1.countDown();
+            cd2.await();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < race.getStages().size(); i++) {
+            race.getStages().get(i).go(this);
+        }
+        if(firstPlace==null){
+            firstPlace = this;
+            System.out.println(getName() + " - WIN");
+        }
+        cd3.countDown();
+    }
+}
